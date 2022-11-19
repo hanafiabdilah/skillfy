c79b4280aae68b88abe55a14121fa7e8cb455650
hanafiabdilah <55283082+hanafiabdilah@users.noreply.github.com>
2022-11-19 16:50:23 +0700

    partner section

--- script.js
+++ script.js
@@ -14,4 +14,23 @@ $(document).ready(() => {
     courseNextButton.click(() => {
         courseCarousel.trigger('next.owl.carousel')
     })
+
+
+    const partnerPrevButton = $(".partner .nav .prev")
+    const partnerNextButton = $(".partner .nav .next")
+    const partnerCarousel = $('.partner .items')
+    partnerCarousel.owlCarousel({
+        center: true,
+        margin: 50,
+        items: 6,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000
+    })
+    partnerPrevButton.click(() => {
+        partnerCarousel.trigger('prev.owl.carousel')
+    })
+    partnerNextButton.click(() => {
+        partnerCarousel.trigger('next.owl.carousel')
+    })
 })
